02d2419e68813af874a976f3e71676e81ae01bbe
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2025-01-23 14:08:40 +0800

    feat: add password length restrictions

--- main.c
+++ main.c
@@ -8,8 +8,9 @@ unsigned char *generate_password(int length)
 {
     unsigned char *password = malloc(length + 1);
     // check that malloc doesn't return NULL (rarely happens, but it's good practice to check for this)
-    if (password == NULL) {
-        printf("ERROR: malloc failed, insufficient memory likely\n");
+    if (password == NULL)
+    {
+        printf("\nERROR: malloc failed, insufficient memory likely\n\n");
         exit(EXIT_FAILURE);
     }
 
@@ -38,13 +39,29 @@ int main(int argc, char *argv[])
     // initialize rand() function using current timestamp
     srand(time(NULL));
 
-    // call generate_password function with default length
-    unsigned char *password = generate_password(DEFAULT_PWD_LENGTH);
+    unsigned char *password;
+
+    // if the user provides a length argument, use provided length; else use default length
+    if (argc > 1)
+    {
+        const int length = atoi(argv[1]);
+        // enforce password length restrictions
+        if (length <= 0 || length > 64)
+        {
+            printf("\nERROR: length cannot be greater than 64\n\n");
+            exit(EXIT_FAILURE);
+        }
+        password = generate_password(length);
+    }
+    else
+    {
+        password = generate_password(DEFAULT_PWD_LENGTH);
+    }
 
     // here the password variable can be manipulated for more advanced features
 
     // free memory allocated for password
     free(password);
 
-    return 0;
+    exit(EXIT_SUCCESS);
 }
